53d7593e2f2680753f62c190eb10c5936d3f273e
akashjethva <akashjethva7085@gmail.com>
2026-01-12 20:42:42 +0530

    chore: configure capacitor for live updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.akashohk1.app',
   appName: 'Payment Soft',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'https://aaryatechnologies.vercel.app',
+    cleartext: true
+  }
 };
 
 export default config;
